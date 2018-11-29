48cf5e7a597ec13120d7661f2dc99ea716f52a58
Axelle Tortai <a.tortai14@gmail.com>
2018-11-30 00:53:13 +0100

    keyyyys

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -2,7 +2,8 @@
 
 Mailjet.configure do |config|
   config.api_key = Rails.application.credentials.mailjet[:public_key]
-  config.secret_key = Rails.application.credentials.mailjet[:secret_key] 
+  config.secret_key = Rails.application.credentials.mailjet[:secret_key]
+  config.secret_key_base =  Rails.application.credentials.secret_key_base 
   config.default_from = "axelle.tortai@student.esc-rennes.com"
   config.api_version = 'v3.1'
 end
