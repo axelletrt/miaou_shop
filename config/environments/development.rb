5391d1f413f1663bef7af2800e787cf0d641bb3a
Axelle Tortai <a.tortai14@gmail.com>
2018-11-30 01:10:22 +0100

    config action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailjet
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
 
 end
