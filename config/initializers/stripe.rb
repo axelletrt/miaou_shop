a27a3ba62fec2f1df4e4af46e8d5d2143bbec7f1
Axelle Tortai <a.tortai14@gmail.com>
2018-11-30 01:02:55 +0100

    last chance

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,3 @@
-# Rails.configuration.stripe = { :access_key_id => Rails.application.credentials.stripe[:access_key_id], :secret_access_key => Rails.application.credentials.stripe[:secret_access_key] }
+Rails.configuration.stripe = { :access_key_id => Rails.application.credentials.stripe[:access_key_id], :secret_access_key => Rails.application.credentials.stripe[:secret_access_key] }
 
-# Stripe.api_key = Rails.configuration.stripe[:secret_access_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_access_key]
